64bd4ed48ca78702acecd734dab4f7773bf72640
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2024-12-17 17:42:24 +0530

    Attempted Program for 17-12-2024

---
+++ 12-17-2024/Digit_Sum_Paritites.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int digitSum(int x){
+    int ans = 0;
+    while(x>0){
+        ans += (x%10);
+        x/=10;
+    }
+    return ans;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(digitSum(n)%2 == digitSum(n+1)%2){
+            cout<<(n+2)<<endl;
+        }
+        else{
+            cout<<(n+1)<<endl;  
+        } 
+    }
+}
